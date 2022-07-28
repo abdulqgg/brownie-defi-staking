079b20e11824e5ce272ebd541d0507a98ec53487
abdul gilani <abdul.gilani@gmail.com>
2022-07-28 16:53:26 +0100

    test contracts added

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,25 @@
-from scripts.helpful_scripts import get_account
+from multiprocessing.spawn import get_command_line
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import DappToken, TokenFarm, network, config
+from web3 import Web3
+
+KEPT_BALANCE = Web3.towei(100, "ether")
 
 def deploy_token_farm_and_dapp_token():
     account = get_account()
+    dapp_token = DappToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        dapp_token.address, 
+        {"from": account}, 
+        publish_source=config["networks"][network.show_active()]["verify"])
+    tx = dapp_token.transfer(token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account})
+    tx.wait(1)
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    add_allowed_tokens(token_farm, sd ,account)
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    pass
 
 def main():
     deploy_token_farm_and_dapp_token()
