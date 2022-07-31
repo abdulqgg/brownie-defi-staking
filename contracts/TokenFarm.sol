ae68f185551a08f9998a554bc7e2256feb4c487a
abdul gilani <abdul.gilani@gmail.com>
2022-07-31 15:31:48 +0100

    tests and conftest

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -32,7 +32,7 @@ contract TokenFarm is Ownable{
         tokenPriceFeedMapping[_token] = _priceFeed;
     }
 
-    function issueTokens(address _user) public onlyOwner {
+    function issueTokens() public onlyOwner {
         for(uint256 stakersIndex=0; stakersIndex < stakers.length; stakersIndex ++){
             address recipient = stakers[stakersIndex];
             uint256 userTotalValue = getUserTotalValue(recipient);
