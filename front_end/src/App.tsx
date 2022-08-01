00c2079dc9f0d79776da12b1a238d23d5735c24f
abdul gilani <abdul.gilani@gmail.com>
2022-08-01 21:16:53 +0100

    header init

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { DAppProvider, ChainId } from "@usedapp/core"
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <DAppProvider config={{
       supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
     }}>
+      <Header />
       <div>Hi!</div>
     </DAppProvider>
   );
