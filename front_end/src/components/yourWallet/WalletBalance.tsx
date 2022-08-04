ad7b3761798b41e82e27273289dea18d77bd9a2b
abdul gilani <abdul.gilani@gmail.com>
2022-08-04 16:26:13 +0100

    balance msg init

--- front_end/src/components/yourWallet/WalletBalance.tsx
+++ front_end/src/components/yourWallet/WalletBalance.tsx
@@ -2,6 +2,7 @@ import { useEthers, useTokenBalance } from "@usedapp/core"
 import { parse } from "path"
 import { Token } from "../Main"
 import {formatUnits} from "@ethersproject/units" 
+import { BalanceMsg } from "../Balancemsg"
 
 export interface WalletBalanceProps {
     token: Token
@@ -12,5 +13,9 @@ export const WalletBalance = ({token}: WalletBalanceProps) => {
     const { account } = useEthers()
     const tokenBalance = useTokenBalance(address, account)
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
-    return (<div>{formattedTokenBalance}</div>)
+    return (<BalanceMsg>
+        label={"Your un-staked ${name} balance"}
+        tokenImgSrc={image}
+        amount={formattedTokenBalance}
+    </BalanceMsg>)
 }
